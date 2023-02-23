e515b07807fc4723d1b8f8ed880913c46c198c9e
NabeelRehman1 <114175801+NabeelRehman1@users.noreply.github.com>
2023-02-23 14:53:31 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const {Musician} = require("./Musician")
+const {sequelize} = require("./db")
+
+const port = 3000;
+
+//TODO
+
+app.get('/musicians', async (req, res) => {
+  const catchmusian = await Musician.findAll()
+    res.json(catchmusian);
+});
+
+
+app.listen(port, () => {
+    sequelize.sync();
+    console.log(`Listening on port ${port}`)
+})
